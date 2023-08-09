349090aaefefbf043e4865afb28e7642e8c58bd0
Leonardo Cascianelli <leo@li.finance>
2023-08-09 15:32:42 +0200

    🚀 initial commit

---
+++ src/ethStaking.ts
@@ -0,0 +1,14 @@
+import { ContractCall } from "@lifi/types"
+import { STETH_ON_MAINNET } from "./commonAddresses"
+import { zeroAddress } from "viem"
+
+export const ethStakingCall = (amount: bigint): ContractCall => {
+    return {
+        fromTokenAddress: zeroAddress,
+        toTokenAddress: STETH_ON_MAINNET,
+        toContractAddress: '0xae7ab96520de3a18e5e111b5eaab095312d7fe84',
+        toContractCallData: '0x',
+        fromAmount: amount.toString(),
+        toContractGasLimit: "110000"
+    }
+}
